5f0917ad95848af8b209188d8aba756e317cf5f9
Nate del Rosario <78570697+nateackerman@users.noreply.github.com>
2021-06-10 13:46:27 -0700

    added overload example

--- final/overload.cpp
+++ final/overload.cpp
@@ -13,6 +13,7 @@ private:
   double _real;
   double _imaginary;
   friend std::ostream &operator<<(std::ostream &os, const Complex &c);
+  friend Complex operator+(const Complex &c1, const Complex &c2);
 
 public:
   Complex(double real, double imaginary) : _real(real), _imaginary(imaginary)
@@ -28,16 +29,23 @@ public:
 
   // overloading the + operator
   // note the const and &
+  // when complex is defined inside the class, you only need one param
   Complex operator+(const Complex &c) const
   {
     return Complex(_real + c._real, _imaginary + c._imaginary);
   }
+
   void dump()
   {
     printf("real, imaginary = ");
     printf("[%.1lf, %.1lf]\n", _real, _imaginary);
   }
 };
+
+Complex operator+(const Complex &c1, const Complex &c2)
+{
+  return Complex(c1._real + c2._real, c1._imaginary + c2._imaginary);
+}
 
 std::ostream &operator<<(std::ostream &os, const Complex &c)
 {
@@ -53,4 +61,5 @@ int main()
 
   Complex c3 = c1.sum(c2);
   Complex c4 = c1 + c2; // operator+()
+  c4.dump();
 }
